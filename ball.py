c17996650e81db2aea7100b7975f893ed967d0bc
darkofff <janek.dzialowski@gmail.com>
2024-12-17 22:33:35 +0100

    backup

--- ball.py
+++ ball.py
@@ -2,13 +2,9 @@ import pygame as py
 import math
 
 class Ball (py.sprite.Sprite):
+
     
-    speedx = 2
-    speedy = 1
-    gravity = -9.81
-    one_meter = 10
-    
-    def __init__(self, x, y):
+    def __init__(self, x, y, GRAVITY):
         super().__init__()
         self.image = py.Surface((14, 14), py.SRCALPHA)
         py.draw.circle(self.image, (0, 0, 0), (7, 7), 7)
@@ -16,10 +12,14 @@ class Ball (py.sprite.Sprite):
         self.rect.center = (x, y)
         self.x = x
         self.y = y
+        self.gravity = GRAVITY
+       
         
     
 
-    def update(self, velocity, angle, tick):
+    def update(self, velocity, angle, tick, meter_size):
+
+        one_meter = int(meter_size)
         
         angle_radians = math.radians(angle)
         velocity_x = math.cos(angle_radians) * velocity
@@ -27,14 +27,14 @@ class Ball (py.sprite.Sprite):
 
         time_in_seconds = tick/60
         
-        distance = self.x + (velocity_x * time_in_seconds) * self.one_meter
-        height = self.y - ( velocity_y * time_in_seconds + (self.gravity * time_in_seconds**2)/2) * self.one_meter
+        distance = self.x + (velocity_x * time_in_seconds) * one_meter
+        height = self.y - ( velocity_y * time_in_seconds + (self.gravity * time_in_seconds**2)/2) * one_meter
 
         if (height<self.y or tick==0):
             self.rect.centerx = math.floor(distance)
             self.rect.centery = math.floor(height)
 
-            return (self.rect.centerx, self.rect.centery, False)
+            return (self.rect.centerx, self.rect.centery, False, (velocity_x, velocity_y))
         else: 
             self.rect.centery = self.y
-            return (self.rect.centerx, self.y, True)
+            return (self.rect.centerx, self.y, True, (velocity_x, velocity_y))
--- main.py
+++ main.py
@@ -8,12 +8,15 @@ py.font.init()  # Initialize Pygame's font system
 
 # VARIABLES
 WIDTH, HEIGHT = 800, 600
-COORDINATE_START_X = 30
+COORDINATE_START_X = 10
 COORDINATE_START_Y = 500
 BALL_RADIUS = 7
+GRAVITY = -9.81
+
 running = True # Dla pętli
-x, y = 0, 600
-speed = 5
+""" x, y = 0, 600 """
+
+
 
 # SETUP
 screen = py.display.set_mode((WIDTH, HEIGHT))
@@ -23,6 +26,7 @@ py.display.set_caption("Rzut ukośny")
 # COLORS
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
+GRAY = (100, 100, 100)
 RED = (255, 0, 0)
 INPUT_BLANK = (200, 200, 200)
 INPUT_SELECTED = (240, 240, 240)
@@ -44,17 +48,29 @@ game_state = 0
 # --- GAME STATE 0 START ---
 
 # INPUTS
-velocity_input_rect = py.Rect(200, 565, 60, 26)  # Position and size of the input field
+
+velocity_input_rect = py.Rect(250, 565, 60, 26)  # Position and size of the input field
 velocity = ""  # Text the user types
 velocity_active = False  # Track if the input is active ( naciśnięty )
 velocity_input_color = INPUT_BLANK
 
-angle_input_rect = py.Rect(200, 535, 60, 26) 
+angle_input_rect = py.Rect(250, 535, 60, 26) 
 angle = ""  
 angle_active = False  
 angle_input_color = INPUT_BLANK
 
-start_button_rect = py.Rect(300, 550, 50, 26)
+grid_density_rect = py.Rect(700, 565, 60, 26) 
+grid_density_coefficient = '10'
+grid_density_active = False
+grid_density_color = INPUT_BLANK
+
+meter_size_rect = py.Rect(700, 535, 60, 26) 
+meter_size = '5'
+meter_size_active = False
+meter_size_color = INPUT_BLANK
+
+
+start_button_rect = py.Rect(350, 550, 50, 26)
 start_button_text = font.render("Rzuć", True, (0, 0, 0)) 
 
 # --- GAME STATE 0 END ---
@@ -62,7 +78,7 @@ start_button_text = font.render("Rzuć", True, (0, 0, 0))
 # --- GAME STATE 1 START ---
 
 # BALL
-ball = Ball(COORDINATE_START_X, COORDINATE_START_Y)
+ball = Ball(COORDINATE_START_X, COORDINATE_START_Y, GRAVITY)
 
 # number of tick while throw
 tick = 0
@@ -73,6 +89,13 @@ tick = 0
 
 x_filnal_pos = 0
 y_filnal_pos = 0
+
+time_total = 0
+total_distance = 0
+max_height = 0
+
+# INPUTS
+
 
 # --- GAME STATE 2 END ---
 
@@ -92,8 +115,10 @@ while running:
             case 0:
                 # INPUTS
                 if event.type == py.MOUSEBUTTONDOWN:
-                    velocity_active = velocity_input_rect.collidepoint(event.pos)  # Activate if clicked inside   
-                    angle_active = angle_input_rect.collidepoint(event.pos)  # Activate if clicked inside   
+                    velocity_active = velocity_input_rect.collidepoint(event.pos)    
+                    angle_active = angle_input_rect.collidepoint(event.pos)  
+                    grid_density_active = grid_density_rect.collidepoint(event.pos)  
+                    meter_size_active = meter_size_rect.collidepoint(event.pos)  
                     if start_button_rect.collidepoint(event.pos):
                         game_state = 1
                 if event.type == py.KEYDOWN and velocity_active:
@@ -106,22 +131,39 @@ while running:
                         angle = angle[:-1]
                     else:
                         angle += event.unicode
+                if event.type == py.KEYDOWN and grid_density_active:
+                    if event.key == py.K_BACKSPACE:
+                        grid_density_coefficient = grid_density_coefficient[:-1]
+                    else:
+                        grid_density_coefficient += event.unicode
+                if event.type == py.KEYDOWN and meter_size_active:
+                    if event.key == py.K_BACKSPACE:
+                        meter_size = meter_size[:-1]
+                    else:
+                        meter_size += event.unicode
             case 1:
                 """ print('angle: ', float(angle))
                 print('velocity: ', float(velocity)) """
 
 
             case 2:
-                print('case 2')
+                """ print('case 2') """
 
-    # BLIT
+    # BLIT UKŁAD WSPÓŁRZĘDNYCH
     screen.blit(background, (0, 0))
-    py.draw.line(screen, BLACK, (COORDINATE_START_X, COORDINATE_START_Y), (COORDINATE_START_X, 0), width=1)
-    py.draw.line(screen, BLACK, (COORDINATE_START_X, COORDINATE_START_Y), (2000, COORDINATE_START_Y), width=1)
     
-    """ screen.blit(sky, (0, 0))
-    screen.blit(ground, (0, 500)) """
    
+    # Potrzebne żeby się nie wywalało jak się wyczyści input dla szerokości siatki. Ustala default na 10m
+    grid_density_coefficient_int = int(grid_density_coefficient) if not grid_density_coefficient=="" else 10
+    meter_size_int = int(meter_size) if not meter_size=="" else 5
+
+    for x in range(COORDINATE_START_X, WIDTH, meter_size_int* grid_density_coefficient_int):
+        py.draw.line(screen, GRAY, (x, COORDINATE_START_Y), (x, 0), 1)  # Linie pionowe
+    for y in range(COORDINATE_START_Y, 0, -meter_size_int* grid_density_coefficient_int):
+        py.draw.line(screen, GRAY, (COORDINATE_START_X, y), (WIDTH, y), 1)  # Linie poziome
+   
+    py.draw.line(screen, BLACK, (COORDINATE_START_X, COORDINATE_START_Y), (COORDINATE_START_X, 0), width=2)
+    py.draw.line(screen, BLACK, (COORDINATE_START_X, COORDINATE_START_Y), (2000, COORDINATE_START_Y), width=2)
 
     match game_state:
         case 0: 
@@ -129,8 +171,10 @@ while running:
             # UPDATE INPUT COLOR
             velocity_input_color = INPUT_BLANK if not velocity_active else  INPUT_SELECTED
             angle_input_color = INPUT_BLANK if not angle_active else  INPUT_SELECTED
+            grid_density_color = INPUT_BLANK if not grid_density_active else  INPUT_SELECTED
+            meter_size_color = INPUT_BLANK if not meter_size_active else INPUT_SELECTED
 
-            # USER INPUT SECTION
+            # ANGLE INPUT
             angle_label = font.render("Kąt (w stopniach): ", True, (0, 0, 0))
             screen.blit(angle_label, (20, 540))
 
@@ -138,34 +182,65 @@ while running:
             angle_text_surface = font.render(angle, True, (0, 0, 0))  
             screen.blit(angle_text_surface, (angle_input_rect.x + 5, angle_input_rect.y + 5))  
 
-            velocity_label = font.render("Prędkość początkowa: ", True, (0, 0, 0))
+            # VELOCITY INPUT
+            velocity_label = font.render("Prędkość początkowa (m/s): ", True, (0, 0, 0))
             screen.blit(velocity_label, (20, 570))
             
             py.draw.rect(screen, velocity_input_color, velocity_input_rect)  
             velocity_text_surface = font.render(velocity, True, (0, 0, 0))  
             screen.blit(velocity_text_surface, (velocity_input_rect.x + 5, velocity_input_rect.y + 5))  
 
+            # THROW BUTTON
             py.draw.rect(screen, (150, 150, 150), start_button_rect)
             screen.blit(start_button_text, (start_button_rect.x+5, start_button_rect.y+5 ))
             
-            py.draw.circle(screen, (0, 0, 0), (30, 500), BALL_RADIUS)
+            # GRID DENSITY
+            grid_density_label = font.render("Gęstość siatki (w metrach): ", True, (0, 0, 0))
+            screen.blit(grid_density_label, (450, 570))
+
+            py.draw.rect(screen, grid_density_color, grid_density_rect)
+            grid_density_text_surface = font.render(grid_density_coefficient, True, (0, 0, 0)) 
+            screen.blit(grid_density_text_surface, (grid_density_rect.x + 5, grid_density_rect.y + 5))  
+           
+            # ROZMIAR 1 METRA
+            meter_size_label = font.render("Długość 1 metra (w px): ", True, (0, 0, 0))
+            screen.blit( meter_size_label, (450, 535))
+
+            py.draw.rect(screen, meter_size_color, meter_size_rect)
+            meter_size_text_surface = font.render(meter_size, True, (0, 0, 0)) 
+            screen.blit(meter_size_text_surface, (meter_size_rect.x + 5, meter_size_rect.y + 5))  
+
+            # BALL 
+            py.draw.circle(screen, (0, 0, 0), (COORDINATE_START_X, COORDINATE_START_Y), BALL_RADIUS)
             
         case 1: 
             # BALL
-            x_pos, y_pos, has_landed = ball.update(float(velocity), float(angle), tick)
+            x_pos, y_pos, has_landed, velocity_tuple = ball.update(float(velocity), float(angle), tick, meter_size)
             screen.blit(ball.image, ball.rect)
 
             x_filnal_pos = x_pos
             y_filnal_pos = y_pos
 
-            # print("pozycja: ", x_pos, y_pos)
+            
             
             tick+=1
 
             if has_landed:
+                velocity_x, velocity_y = velocity_tuple
+                time_total = (2*velocity_y)/-GRAVITY
+                total_distance = velocity_x * time_total
+                # max_height = 
+
+                
                 game_state = 2
         case 2: 
             py.draw.circle(screen, (0, 0, 0), (x_filnal_pos, y_filnal_pos), BALL_RADIUS)
+
+            total_time_display = font.render(f"Całkowity czas lotu: {round(time_total, 4)} s", True, (0, 0, 0))
+            screen.blit(total_time_display, (20, 535))
+            
+            total_distance_display = font.render(f"Odległość: {round(total_distance, 4)} m", True, (0, 0, 0))
+            screen.blit(total_distance_display, (20, 575))
         case _:
             running = False
 
